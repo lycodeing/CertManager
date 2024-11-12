037772f5a2a488ac92f36acd13ee267cd773667e
lycodeing <jiyishiweiyideing@gmail.com>
2024-11-12 23:09:04 +0800

    perf:mybatis plus 配置

--- cert-web/src/main/java/cn/lycodeing/cert/web/CertApplication.java
+++ cert-web/src/main/java/cn/lycodeing/cert/web/CertApplication.java
@@ -1,8 +1,10 @@
 package cn.lycodeing.cert.web;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@MapperScan(basePackages = "cn.lycodeing.cert.web.mapper")
 @SpringBootApplication
 public class CertApplication {
     public static void main(String[] args) {
